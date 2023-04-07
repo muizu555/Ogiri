b9e831db29d566a977082ab0589ea6f7c2baceb4
muizu <muizu0920555@protonmail.com>
2023-04-07 19:30:07 +0900

    change

--- routes/getPostsSorted.js
+++ routes/getPostsSorted.js
@@ -3,50 +3,66 @@ const router = express.Router();
 
 const Post = require("../models/Post");
 
-//まず関数を作る
-//人気順
-const getPostByLikes = async () => {//データベースをやりとりしている。
+//まず関数を作る　　//ここ激ムズ MongooseのDocumentもっと見る likesが配列だったので、うまくソートできなかったっぽい,_idも一緒に入れ替えてるっぽい  あってる？
+// //人気順
+ const getPostsByLikes = async () => {
+     try {
+      const posts = await Post.find({}).sort({likes: -1, _id: -1})
+      return posts;
+     } catch (err) {
+       console.log(err);
+     }
+   };
+  
+  router.get("/byLikes", async(req, res) => {
     try {
-        const posts = await Post.find({}).sort({ "likes.length": -1 });
-        return posts;
+      const posts = await getPostsByLikes();
+      return res.status(200).json(posts);
     } catch (err) {
-        console.log(err);
+      return res.status(500).json("Server Error");
     }
-};
+  });  
 
-//Most recent
-const getPostByDate = async () => {
-    try {
-        const posts = await Post.find({}).sort({ createdAt: -1 });
-        return posts;
-    } catch (err) {
-        console.log(err);
+// //Most recent
+ const getPostByDate = async () => {
+     try {
+         const posts = await Post.find({}).sort({ createdAt: -1 });
+         return posts;
+     } catch (err) {
+         console.log(err);
     }
-}
+ }
 
 
 
 
-//人気順
-router.get("/sortByLikes", async(req, res) => {
-    try {
-        const posts = await getPostsByLikes();
-        return res.status(200).json(posts);
-    }
-    catch(err){
-        return res.status(500).json("Server Error");
-    }
-});
 
-//Most recent
-router.get("/sortByLate", async(req, res) => {
-    try {
-        const posts = await getPostByDate();
-        return res.status(200).json(posts);
-    } catch (err) {
-        return res.status(500).json("Server Error");
-    }
-})
+ //Most recent
+ router.get("/byDate", async(req, res) => {
+     try {
+         const posts = await getPostByDate();
+         return res.status(200).json(posts);
+     } catch (err) {
+         return res.status(500).json("Server Error");
+     }
+ })
 
 
 module.exports = router;
+
+
+
+
+
+// const getPostsByLikes = async () => {//ここ激ムズ MongooseのDocumentもっと見る likesが配列だったので、うまくソートできなかったっぽい
+//     try {
+//       const posts = await Post.aggregate([
+//         {
+//           $sort: { likes: -1, _id: -1 } // likes の配列の長さで降順ソートし、_id で降順に並べ替える
+//         },
+//       ]);
+//       return posts;
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   };
