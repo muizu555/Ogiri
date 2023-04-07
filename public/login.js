4a59a3cbbfac6fef8ad2f9fa67a23e080845580f
muizu <muizu0920555@protonmail.com>
2023-04-07 10:09:35 +0900

    change

---
+++ public/login.js
@@ -0,0 +1,28 @@
+const formDOM = document.querySelector(".task-form");
+const usernameDOM = document.querySelector(".task-input");
+const passwordDOM = document.querySelector(".content-input");
+
+//console.log(axios);
+
+
+
+
+formDOM.addEventListener("submit", async (event) => {
+    event.preventDefault();//not reload
+    const username = usernameDOM.value;
+    const password = passwordDOM.value;
+
+     console.log(usernameDOM.value);
+     console.log(passwordDOM.value);
+
+    try {
+        await axios.post("/api/auth/login", { username: username, password: password});
+        usernameDOM.value = "";
+        passwordDOM.value="";
+        window.location.href = "index.html";
+    } catch (err) {
+        console.log(err);
+    }
+
+})
+
