f9fb28cc840dc7f6696728de6c947180c65dcfb5
muizu <muizu0920555@protonmail.com>
2023-04-11 10:56:47 +0900

    hash

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,8 @@ const bcrypt = require("bcrypt");
 
 
 
-//register ユーザー登録
+
+//register ユーザー登録 /ok
 router.post("/register", async(req,res) => {
     try {
         const { username, password } = req.body;
@@ -17,24 +18,24 @@ router.post("/register", async(req,res) => {
             username: username,
             password: hash,
         });
-
-
         const user = await newUser.save();//ここで登録を保存している。documentを見るべし。ここポイント
         return res.status(200).json(user);
-
-
     } catch (err) {
         return res.status(500).json(err.message);//500はサーバー関連のerr
     }
 });
 
-//login 
+//login /ok //ログインで重要なことはログインの状態を保つことなのでまだ気合いれよう
 router.post("/login", async(req, res) => {
     try {
         const user = await User.findOne({username: req.body.username});//mongooseのドキュメントをみろ。
-        if(!user) return res.status(404).json("ユーザーが見つかりません");//ここらへんでbcuriptなのでハッシュ化する方が良い
-        const vailedPassword = req.body.password === user.password;//req.body.password === user.passwordが成り立てば、trueが返される。前のfindで配列は確定済み
-        if(!vailedPassword) return res.status(400).json("パスワードが違います");
+        if(!user) return res.status(404).json("ユーザーが見つかりません");
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        //const validPassword = req.body.password === user.password;//req.body.password === user.passwordが成り立てば、trueが返される。前のfindで配列は確定済み
+        if(validPassword) {//ここから
+            req.session.user_id = user._id;
+            
+        }
 
         //二つの条件式を突破できたら、
         return res.status(200).json(user);
